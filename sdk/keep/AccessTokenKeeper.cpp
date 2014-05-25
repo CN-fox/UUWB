66dfffa904fb67b8b13b8092fde584b9a516d63b
qu.wang <wangqu.0913@gmail.com>
2014-05-26 00:42:07 +0800

    implement the method keep()

--- sdk/keep/AccessTokenKeeper.cpp
+++ sdk/keep/AccessTokenKeeper.cpp
@@ -1,4 +1,5 @@
 #include"AccessTokenKeeper.h"
+#include<QJsonDocument>
 #include<QFile>
 
 AccessTokenKeeper::AccessTokenKeeper()
@@ -6,18 +7,29 @@ AccessTokenKeeper::AccessTokenKeeper()
 
 }
 
-void AccessTokenKeeper::keep(WeiBo* wiebo)
+void AccessTokenKeeper::keep(WeiBo* weibo)
 {
+    QVariantMap map;
+    map.insert("uid",weibo->getUid());
+    map.insert("token",weibo->gettoken());
+    map.insert("expires_in",weibo->getExpiresIn());
+    QJsonObject json = QJsonObject::fromVariantMap(map);
+    QJsonDocument document = QJsonDocument(json);
     QFile data("save");
     if (data.open(QFile::WriteOnly | QFile::Truncate)) {
         QTextStream out(&data);
-        out << "test";
+        out << document.toJson();
     }
 }
 
 void AccessTokenKeeper::clear()
 {
-
+    QFile data("save");
+    if (data.exists()) {
+        data.open(QFile::WriteOnly | QFile::Truncate);
+        QTextStream out(&data);
+        out << "";
+    }
 }
 
 void AccessTokenKeeper::read(WeiBo* weibo)
